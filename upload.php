789513eaf2f54e32873f98bea6f218a778690f8a
Christophe Nshimiyimana <ragonartsofficial@gmail.com>
2024-02-26 14:08:50 +0200

    Add files via upload

---
+++ upload.php
@@ -0,0 +1,32 @@
+<?php 
+// Specify the directory where the uploaded files will be stored
+$uploadDirectory = 'img/';
+
+// Check if a file was uploaded
+if (isset($_FILES['file']) && $_FILES['file']['error'] === UPLOAD_ERR_OK) {
+    $file = $_FILES['file'];
+    
+    // Validate the file (example: allow only image files)
+    $allowedTypes = ['image/jpeg', 'image/png'];
+    if (!in_array($file['type'], $allowedTypes)) {
+        echo 'Error: Only JPEG and PNG images are allowed.';
+        exit;
+    }
+    
+    // Generate a unique file name
+    $fileName = uniqid() . '_' . $file['name'];
+    
+    // Move the uploaded file to the desired location
+    if (move_uploaded_file($file['tmp_name'], $uploadDirectory . $fileName)) {
+        echo 'File uploaded successfully.';
+    } else {
+        echo 'Error uploading file.';
+    }
+} else {
+    echo 'No file uploaded.';
+}
+
+
+
+
+?>
